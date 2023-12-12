b31c6a12efd059544fd4d10b51699f97b5c5239c
nadav azaria <nadav.azaria98@gmail.com>
2023-12-12 15:03:10 +0200

    name_chage

--- myEnv.py
+++ myEnv.py
@@ -1,12 +1,13 @@
 from flask import *
 from icecream import ic 
+import os
 
-
+def clear() :
+    lambda: os.system('cls')
 def main():
     for i in range(0,100,3):
         ic( f"{i} is devisable by 3")
 
-
-
 if __name__ == "__main__":
-    main()
+    # main()
+    clear()
